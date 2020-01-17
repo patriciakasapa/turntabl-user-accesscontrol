c5b97e153ddbdfdfa766ff06f797cf6d74fed0e1
patriciakasapa <patriciakasapa07@gmail.com>
2020-01-17 19:48:55 +0000

    heroku

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/usermanagementaws'));
+app.use(express.static(__dirname + 'dist/UserManagementAWS'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/usermanagementaws/index.html'));
+res.sendFile(path.join(__dirname+'dist/UserManagementAWS/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
