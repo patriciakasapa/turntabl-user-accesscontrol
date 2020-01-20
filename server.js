a9c08c089dfe4954415c0db8f8badd6f63889f66
patriciakasapa <patriciakasapa07@gmail.com>
2020-01-20 17:40:53 +0000

    saml auth

--- server.js
+++ server.js
@@ -19,23 +19,27 @@
 //     console.log('on port 8081');
 // });
 
-const SamlStrategy = require('passport-saml').Strategy;
+const SamlStrategy = require("passport-saml").Strategy;
 
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const passport = require("passport");
 
+let userEmail = process.env.ADMIN_EMAIL
 passport.use(new SamlStrategy(
     {
-      path: 'https://turntabl-user-accesscontrol.herokuapp.com/',
+      protocol: "https://",
+      path: '/auth/saml/callback',
       entryPoint: process.env.ENTRY_POINT,
-      issuer: 'passport-saml'
+      issuer: 'passport-saml',
+      path: "/auth/saml/callback"
+    //   cert: process.env.CERTIFICATE
     },
     
     function(profile, done) {
       findByEmail(profile.email, function(err) {
-        userEmail = profile.nameID;
+        userEmail;
         if (err) {
           return done(err);
         }
@@ -58,10 +62,10 @@ passport.use(new SamlStrategy(
 const app = express();
 
 app.get(
-    "/https://accounts.google.com/AccountChooser?continue=https://accounts.google.com/o/saml2/initsso?idpid%3DC00n1isy6%26spid%3D350630521091%26forceauthn%3Dfalse%26from_login%3D1%26as%3DbfUSj1vtW0E47VOaHGeZ1g&ltmpl=popup&btmpl=authsub&scc=1&oauth=1",
+    "/login",
     passport.authenticate("saml", {
       successRedirect: "/",
-      failureRedirect: "https://accounts.google.com/AccountChooser?continue=https://accounts.google.com/o/saml2/initsso?idpid%3DC00n1isy6%26spid%3D350630521091%26forceauthn%3Dfalse%26from_login%3D1%26as%3DbfUSj1vtW0E47VOaHGeZ1g&ltmpl=popup&btmpl=authsub&scc=1&oauth=1"
+      failureRedirect: "/login"
     })
   );
   
@@ -72,6 +76,22 @@ app.get(
     // res.end("You have logged out.");
 //   });
 
+app.post(
+  "/auth/saml/callback",
+  passport.authenticate("saml", {
+    failureRedirect: "/error",
+    failureFlash: false
+  })
+  );
+
+  app.all("*", function (req, res, next) {
+    if (req.isAuthenticated() || process.env.NODE_ENV !== "production") {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  });
+
 // Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/dist/UserManagementAWS'));
 
@@ -81,4 +101,4 @@ res.sendFile(path.join(__dirname+'/dist/UserManagementAWS/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8050);
