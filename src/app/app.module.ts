281c6a89a75b691278c73f6db17a307e2f456898
patriciakasapa <patriciakasapa07@gmail.com>
2020-01-21 10:10:33 +0000

    saml loks positive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VerificationComponent } from './verification/verification.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VerificationComponent
+    VerificationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
